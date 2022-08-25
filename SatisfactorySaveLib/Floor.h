7b08b057017984063470e6b03b29f76b70fc57ed
daniel.pal <pdany86@gmail.com>
2022-08-25 11:19:14 +0200

    ...

--- SatisfactorySaveLib/Floor.h
+++ SatisfactorySaveLib/Floor.h
@@ -9,9 +9,30 @@ namespace factorygame {
 
 	class Floor {
 	public:
+		void addObject(std::unique_ptr<GameObject> object);
+
 	private:
 		float _elevation = 0.0f;
+		int _foundationWidth = 10;
+		int _foundationHeight = 10;
 		std::vector<std::unique_ptr<GameObject>> _objects;
+	};
+
+	class FactoryBuilding {
+	public:
+		void addFloor(int foundationWidth, int foundationHeight);
+		void addFloor(std::unique_ptr<Floor> floor);
+		Floor& getFloor(int ix);
+		int floorCount() const { return _floors.size(); }
+
+	private:
+		std::vector<std::unique_ptr<Floor>> _floors;
+	};
+
+
+	class FactoryBuildingBuilderExample {
+	public:
+		static std::unique_ptr<FactoryBuilding> build();
 	};
 
 }
